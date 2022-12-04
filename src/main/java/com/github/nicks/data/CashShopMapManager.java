54631fd0c001386875956891245c8e74d26b4eac
idkNicks <yangdaeyeong0808@gmail.com>
2022-12-04 15:50:17 +0900

    🛠️ 캐시상점 데이터 저장소 완성

--- src/main/java/com/github/nicks/data/CashShopMapManager.java
+++ src/main/java/com/github/nicks/data/CashShopMapManager.java
@@ -1,5 +1,6 @@
 package com.github.nicks.data;
 
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
 import java.util.HashMap;
@@ -7,6 +8,6 @@ import java.util.Map;
 
 public class CashShopMapManager {
 
-    public static Map<String, Inventory> cashShop = new HashMap<>();
-
+    public static Map<String, Inventory> shopMap = new HashMap<>();
+    public static Map<Player, GuiType> typeMap = new HashMap<>();
 }
