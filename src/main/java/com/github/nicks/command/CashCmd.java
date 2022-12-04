40c856a2bd2a88119b3b17926448ae98450f0e6b
idkNicks <yangdaeyeong0808@gmail.com>
2022-12-04 15:46:07 +0900

    🛠️ 캐시 명령어 틀 제작 | 지급, 차감, 설정, 확인, 모두지급, 초기화

--- src/main/java/com/github/nicks/command/CashCmd.java
+++ src/main/java/com/github/nicks/command/CashCmd.java
@@ -5,6 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
 import org.jetbrains.annotations.NotNull;
 
 
