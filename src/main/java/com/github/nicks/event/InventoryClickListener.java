5e202489b2a116aff536611797d048aef6abc99f
idkNicks <yangdaeyeong0808@gmail.com>
2022-12-04 20:40:45 +0900

    🛠️ 인벤토리 클릭 이벤트 | 64개 구매,판매 기능 완성

--- src/main/java/com/github/nicks/event/InventoryClickListener.java
+++ src/main/java/com/github/nicks/event/InventoryClickListener.java
@@ -8,6 +8,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
 
 import java.io.File;
 import java.util.Arrays;
@@ -57,40 +58,61 @@ public class InventoryClickListener implements Listener {
             ConfigUtils config = new ConfigUtils("shop/" + file2.getName().substring(0, file2.getName().length() - 4), plugin);
             if (event.getView().getTitle().equals(config.getString("cashshop.inv.title"))) {
                 if (guiType.containsKey(player) && guiType.get(player) == GuiType.OPEN) {
+                    if(event.getCurrentItem() == null) return;
 
-                    // 캐시상점 구매
+                    if (config.getDouble("cashshop.inv.items." + event.getSlot() + ".Price.buy") == -1) {
+                        System.out.println((event.getClick() == ClickType.LEFT ? "구매" : "판매") + " 불가능한 상품입니다.");
+
+                        return;
+                    }
+
+                    // 캐시상점 1개 구매
                     if (event.getClick() == ClickType.LEFT) {
-                        if (config.getDouble("cashshop.inv.items." + event.getSlot() + ".Price.buy") == -1) {
-                            System.out.println("구매 불가능한 상품입니다.");
-
+                        if (data.getDouble("playerdata.cash") < config.getDouble("cashshop.inv.items." + event.getSlot() + ".Price.buy")) {
+                            System.out.println("돈이 부족합니다.");
                         } else {
-                            if (data.getDouble("playerdata.cash") < config.getDouble("cashshop.inv.items." + event.getSlot() + ".Price.buy")) {
-                                System.out.println("돈이 부족합니다.");
+                            if (!isInventoryFull(player)) {
+                                player.getInventory().addItem(event.getCurrentItem()); // 인벤토리에 아이템 지급
+                                cashAPI.withdrawCash(player, config.getDouble("cashshop.inv.items." + event.getSlot() + ".Price.buy")); // 캐시 차감
                             } else {
-                                if (!isInventoryFull(player)) {
-                                    player.getInventory().addItem(event.getCurrentItem()); // 인벤토리에 아이템 지급
-                                    cashAPI.withdrawCash(player, config.getDouble("cashshop.inv.items." + event.getSlot() + ".Price.sell")); // 캐시 차감
-                                } else {
-                                    player.sendMessage("인벤토리가 가득차서 구매가 불가능합니다!");
-                                }
+                                player.sendMessage("인벤토리가 가득차서 구매가 불가능합니다!");
                             }
-
-
                         }
 
-                        // 캐시상점 판매
+                        // 캐시상점 1개 판매
                     } else if (event.getClick() == ClickType.RIGHT) {
-                        if (config.getDouble("cashshop.inv.items." + event.getSlot() + ".Price.sell") == -1) {
-                            System.out.println("판매 불가능한 상품입니다.");
-
+                        if (Arrays.asList(player.getInventory().getContents()).contains(event.getCurrentItem())) { // 플레이어 인벤토리에 아이템이 있는지 체크
+                            player.getInventory().removeItem(event.getCurrentItem()); // 아이템 제거
+                            cashAPI.depositCash(player, config.getDouble("cashshop.inv.items." + event.getSlot() + ".Price.sell")); // 캐시 추가
+                            System.out.println(event.getSlot() + "의 판매 가격은 " + config.getDouble("cashshop.inv.items." + event.getSlot() + ".Price.sell") + "입니다.");
                         } else {
-                            if (Arrays.asList(player.getInventory().getContents()).contains(event.getCurrentItem())) { // 플레이어 인벤토리에 아이템이 있는지 체크
-                                player.getInventory().removeItem(event.getCurrentItem()); // 아이템 제거
-                                cashAPI.depositCash(player, config.getDouble("cashshop.inv.items." + event.getSlot() + ".Price.sell")); // 캐시 추가
-                                System.out.println(event.getSlot() + "의 판매 가격은 " + config.getDouble("cashshop.inv.items." + event.getSlot() + ".Price.sell") + "입니다.");
+                            player.sendMessage("아이템이 없어 판매가 불가능합니다!");
+                        }
+                    }
+
+                    // 캐시상점 64개 구매
+                    if (event.getClick() == ClickType.SHIFT_LEFT) {
+                        if (data.getDouble("playerdata.cash") * 64 < config.getDouble("cashshop.inv.items." + event.getSlot() + ".Price.buy") * 64) {
+                            System.out.println("돈이 부족합니다.");
+                        } else {
+                            if (!isInventoryFull(player)) {
+                                player.getInventory().addItem(new ItemStack(event.getCurrentItem().getType(), 64)); // 인벤토리에 아이템 지급
+                                cashAPI.withdrawCash(player, config.getDouble("cashshop.inv.items." + event.getSlot() + ".Price.buy") * 64); // 캐시 차감
                             } else {
-                                player.sendMessage("아이템이 없어 판매가 불가능합니다!");
+                                player.sendMessage("인벤토리가 가득차서 구매가 불가능합니다!");
                             }
+                        }
+                    }
+
+                    // 캐시상점 64개 판매
+                    if (event.getClick() == ClickType.SHIFT_RIGHT) {
+                        if (Arrays.asList(player.getInventory().getContents()).contains(new ItemStack(event.getCurrentItem().getType(), 64))) { // 플레이어 인벤토리에 아이템이 있는지 체크
+                            player.getInventory().removeItem(new ItemStack(event.getCurrentItem().getType(), 64)); // 아이템 제거
+                            cashAPI.depositCash(player, config.getDouble("cashshop.inv.items." + event.getSlot() + ".Price.sell") * 64); // 캐시 추가
+                            System.out.println(event.getSlot() + "의 판매 가격은 " + config.getDouble("cashshop.inv.items." + event.getSlot() + ".Price.sell") * 64 + "입니다.");
+                        } else {
+                            player.sendMessage("아이템이 없어 판매가 불가능합니다!");
+
                         }
                     }
                 }
@@ -101,6 +123,7 @@ public class InventoryClickListener implements Listener {
 
     /**
      * 플레이어 인벤토리가 가득찼는지 체크 합니다.
+     *
      * @param player
      * @return
      */
