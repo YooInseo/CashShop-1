cab1e285657adb6f60729356ce84ab24a48840c0
idkNicks <yangdaeyeong0808@gmail.com>
2022-12-04 15:50:31 +0900

    🛠️ 캐시상점 데이터 저장소 완성

--- src/main/java/com/github/nicks/data/CashShopData.java
+++ src/main/java/com/github/nicks/data/CashShopData.java
@@ -3,9 +3,11 @@ package com.github.nicks.data;
 import com.github.nicks.utils.ConfigUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import static com.github.nicks.CashShop.plugin;
-import static com.github.nicks.data.CashShopMapManager.cashShop;
+import static com.github.nicks.data.CashShopMapManager.shopMap;
+import static com.github.nicks.data.CashShopMapManager.typeMap;
 
 public class CashShopData {
 
@@ -17,14 +19,14 @@ public class CashShopData {
      *
      * @param name 캐시상점 이름
      */
-    public void createCashShop(Player player, String name) {
+    public void createCashShop(@NotNull Player player, String name) {
         if (player.isOp()) {
             config = new ConfigUtils("shop/" + name, plugin);
             config.setString("cashshop.name", name);
             config.setString("cashshop.inv.title", name);
             config.setInteger("cashshop.inv.size", 54);
             player.sendMessage("캐시상점 생성 완료");
-            cashShop.put(name, Bukkit.createInventory(null, 54, name));
+            shopMap.put(name, Bukkit.createInventory(null, 54, name));
         }
     }
 
@@ -34,7 +36,7 @@ public class CashShopData {
      *
      * @param name 캐시상점 이름
      */
-    public void deleteCashShop(Player player, String name) {
+    public void deleteCashShop(@NotNull Player player, String name) {
         if (player.isOp()) {
             config = new ConfigUtils("shop/" + name, plugin);
             config.deleteFile();
@@ -48,10 +50,12 @@ public class CashShopData {
      *
      * @param name 캐시상점 이름
      */
-    public void editCashShop(Player player, String name) {
+    public void editCashShop(@NotNull Player player, String name) {
+
         if (player.isOp()) {
             config = new ConfigUtils("shop/" + name, plugin);
-            player.openInventory(cashShop.get(name));
+            player.openInventory(shopMap.get(name));
+            typeMap.put(player, GuiType.EDIT);
         }
     }
 
@@ -61,8 +65,9 @@ public class CashShopData {
      * @param player
      * @param name
      */
-    public void openCashShop(Player player, String name) {
+    public void openCashShop(@NotNull Player player, String name) {
         config = new ConfigUtils("shop/" + name, plugin);
-        player.openInventory(cashShop.get(name));
+        player.openInventory(shopMap.get(name));
+        typeMap.put(player, GuiType.OPEN);
     }
 }
