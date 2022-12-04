856882d718af464b1aa09c91802c502530b452a9
idkNicks <yangdaeyeong0808@gmail.com>
2022-12-04 15:47:05 +0900

    🛠️ 캐시상점 명령어 틀 제작 | 도움말, 생성, 제거, 편집, 열기

--- src/main/java/com/github/nicks/command/CashShopCmd.java
+++ src/main/java/com/github/nicks/command/CashShopCmd.java
@@ -5,9 +5,11 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class CashShopCmd implements CommandExecutor {
+
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
